8f82deb63f2e74735196a71d08bb151e0631318f
jorgemz <jorge_mz@icloud.com>
2021-05-18 16:07:22 -0500

    - property wrapper validate empty and sanitize white space

--- 03-sanitize-and-validate-data/Starter/LongDriveLog/PropertyWrappers/ValidatableState.swift
+++ 03-sanitize-and-validate-data/Starter/LongDriveLog/PropertyWrappers/ValidatableState.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2021 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,55 +32,79 @@
 
 import SwiftUI
 
-struct Shake: GeometryEffect {
-  var amount: CGFloat = 10
-  var shakesPerUnit = 3
-  var animatableData: CGFloat
-
-  func effectValue(size: CGSize) -> ProjectionTransform {
-    ProjectionTransform(CGAffineTransform(
-      translationX: amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
-      y: 0))
-  }
+protocol  Validation {
+  associatedtype Value
+  
+  func validate(_ value: Value) -> Bool
+  
+  func sanitize(_ value: Value) -> Value
 }
 
-struct UpdateNameView: View {
-  @Binding var showModal: Bool
+struct WhitespaceValidation: Validation {
+  struct Options: OptionSet {
+    let rawValue: Int
 
-  @ObservedObject
-  var model: GolfModel
-
-  @ValidatableState(validations: WhitespaceValidation.all)
-  var name: String = ""
-  @State var animateCounter = 0
-
-  var body: some View {
-    NavigationView {
-      VStack {
-        Text("Input your name:")
-        TextField(model.name, text: $name)
-          .padding()
-          .textFieldStyle(RoundedBorderTextFieldStyle())
-          .modifier(Shake(animatableData: CGFloat(animateCounter)))
-          .navigationBarTitle("Update name", displayMode: .inline)
-          .navigationBarItems(leading: Button("cancel") {
-            self.showModal = false
-          }.disabled(model.name.isEmpty), trailing: Button("save") {
-            if self._name.valid {
-              withAnimation { self.animateCounter += 1 }
-              return
-            }
-            self.model.name = self._name.sanitizedValue
-            self.showModal = false
-          })
+    static let trailing = Options(rawValue: 1 << 0)
+    static let leading = Options(rawValue: 1 << 1)
+    
+    static let all: Options = [.leading, .trailing]
+  }
+  
+  typealias Value = String
+  let options: Options
+  
+  func validate(_ value: String) -> Bool {
+    return !value.isEmpty
+  }
+  
+  func sanitize(_ value: String) -> String {
+    var value = value
+    if self.options.contains(.leading) {
+      while value.first?.isWhitespace == true {
+        value.removeFirst()
       }
     }
+    if self.options.contains(.trailing) {
+      while value.last?.isWhitespace == true {
+        value.removeLast()
+      }
+    }
+    return value
   }
+  
+  static let trailing = WhitespaceValidation(options: .leading)
+  static let leading = WhitespaceValidation(options: .trailing)
+  static let all = WhitespaceValidation(options: .all)
 }
 
-struct UpdateNameView_Previews: PreviewProvider {
-  @State static var show: Bool = false
-  static var previews: some View {
-    UpdateNameView(showModal: $show, model: .init())
+@propertyWrapper
+struct ValidatableState<Value, V>: DynamicProperty where V: Validation, V.Value == Value {
+  @State private var underlying: Value
+  private let validations: [V]
+  
+  init(wrappedValue: Value, validations: V...) {
+    self._underlying = State(wrappedValue: wrappedValue)
+    self.validations = validations
+  }
+  
+  var wrappedValue: Value {
+    get { underlying }
+    nonmutating set { underlying = newValue }
+  }
+  
+  var projectedValue: Binding<Value> {
+    $underlying
+  }
+  
+  var sanitizedValue: Value {
+    self.validations.reduce(self.wrappedValue) { $1.sanitize($0) }
+  }
+
+  var valid: Bool {
+    self.validations.allSatisfy { $0.validate(self.sanitizedValue) }
+  }
+  
+  mutating func update() {
+    _underlying.update()
   }
 }
